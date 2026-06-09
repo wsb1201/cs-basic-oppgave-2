ff0b8b7d5eb5ee4be01855a8b4e2ac8ff71ff41d
wsb1201 <wsb291201@gmail.com>
2026-06-09 11:22:30 +0200

    Time-based greetings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var timestamp = DateTime.Now;
+switch (timestamp.Hour)
+{
+    case >= 2 and < 5:
+        Console.WriteLine("Hva driver du med så sent på natten?");
+        break;
+    case >= 5 and < 9:
+        Console.WriteLine("God morgen!");
+        break;
+    case >= 9 and < 12:
+        Console.WriteLine("God formiddag!");
+        break;
+    case >= 12 and < 18:
+        Console.WriteLine("God ettermiddag!");
+        break;
+    case >= 18 or < 2:
+        Console.WriteLine("God kveld!");
+        break;
+}
