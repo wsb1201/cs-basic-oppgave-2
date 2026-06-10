ddf42d4a9d9333a073735d986ec36f2e84e22199
wsb1201 <wsb291201@gmail.com>
2026-06-10 12:11:19 +0200

    Include user-provided name.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
-﻿var timestamp = DateTime.Now;
+﻿Console.Write("Enter name: ");
+string? userName = Console.ReadLine();
+
+var timestamp = DateTime.Now;
 switch (timestamp.Hour)
 {
     case >= 2 and < 5:
         Console.WriteLine("Hva driver du med så sent på natten?");
         break;
     case >= 5 and < 9:
-        Console.WriteLine("God morgen!");
+        Console.WriteLine($"God morgen, {userName}!");
         break;
     case >= 9 and < 12:
-        Console.WriteLine("God formiddag!");
+        Console.WriteLine($"God formiddag, {userName}!");
         break;
     case >= 12 and < 18:
-        Console.WriteLine("God ettermiddag!");
+        Console.WriteLine($"God ettermiddag, {userName}!");
         break;
     case >= 18 or < 2:
-        Console.WriteLine("God kveld!");
+        Console.WriteLine($"God kveld, {userName}!");
         break;
 }
