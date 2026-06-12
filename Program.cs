74c1df36e69498a8cf61989c4f9046e4d392961e
wsb1201 <wsb291201@gmail.com>
2026-06-12 15:00:34 +0200

    Cleanup

--- Program.cs
+++ Program.cs
@@ -1,22 +1,17 @@
 ﻿Console.Write("Enter name: ");
 string? userName = Console.ReadLine();
 
-var timestamp = DateTime.Now;
-switch (timestamp.Hour)
+DateTime now = DateTime.Now;
+
+string timeGreeting = now.Hour switch
 {
-    case >= 2 and < 5:
-        Console.WriteLine("Hva driver du med så sent på natten?");
-        break;
-    case >= 5 and < 9:
-        Console.WriteLine($"God morgen, {userName}!");
-        break;
-    case >= 9 and < 12:
-        Console.WriteLine($"God formiddag, {userName}!");
-        break;
-    case >= 12 and < 18:
-        Console.WriteLine($"God ettermiddag, {userName}!");
-        break;
-    case >= 18 or < 2:
-        Console.WriteLine($"God kveld, {userName}!");
-        break;
-}
+    >= 5 and < 9 => "God morgen",
+    >= 9 and < 12 => "God formiddag",
+    >= 12 and < 18 => "God ettermiddag",
+    _ => "God kveld",
+};
+
+if (string.IsNullOrWhiteSpace(userName))
+    Console.WriteLine($"{timeGreeting}!");
+else
+    Console.WriteLine($"{timeGreeting}, {userName}!");
