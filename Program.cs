a932ab4a5fe55f102a205dbc40c0276451396cc9
wsb1201 <wsb291201@gmail.com>
2026-06-12 15:18:17 +0200

    Special greeting on weekends

--- Program.cs
+++ Program.cs
@@ -3,8 +3,12 @@ string? userName = Console.ReadLine();
 
 DateTime now = DateTime.Now;
 
+DayOfWeek[] weekendDays = [DayOfWeek.Friday, DayOfWeek.Saturday, DayOfWeek.Sunday];
+bool isWeekend = weekendDays.Contains(now.DayOfWeek);
+
 string timeGreeting = now.Hour switch
 {
+    _ when isWeekend => "God helg",
     >= 5 and < 9 => "God morgen",
     >= 9 and < 12 => "God formiddag",
     >= 12 and < 18 => "God ettermiddag",
